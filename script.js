adaa129e143fe91f4f8a588dc6ea9e0bbb2ede87
Alexander <asbulba09@gmail.com>
2025-05-24 20:21:40 +1000

    Update site contents
    Add projects
    Add more info

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 x = 0;
 setInterval(()=>{
-   $("#page").css("background-image", "linear-gradient("+(x++)+"deg, rgba(2,0,36,1) 0%, rgba(202,1,128,1) 50%)");
+   $("#bg").css("background-image", "linear-gradient("+(x++)+"deg, rgba(2,0,36,1) 0%, rgba(202,1,128,1) 50%)");
 },
             25);
